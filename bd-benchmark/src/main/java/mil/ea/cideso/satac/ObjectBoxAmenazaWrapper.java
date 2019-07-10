41b3ce5ea6abff14e1877c725c7561db43f95bbf
Diego <die.perri@gmail.com>
2019-07-10 18:49:35 -0300

    Corregido ObjectBoxAmenazaWrapper

--- bd-benchmark/src/main/java/mil/ea/cideso/satac/ObjectBoxAmenazaWrapper.java
+++ bd-benchmark/src/main/java/mil/ea/cideso/satac/ObjectBoxAmenazaWrapper.java
@@ -52,4 +52,12 @@ public class ObjectBoxAmenazaWrapper {
         this.leido = leido;
     }
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
 }
