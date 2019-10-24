af365e5c8d4df6b5d2840deffac32c7f738aecff
Diego <die.perri@gmail.com>
2019-10-24 20:39:07 -0300

    Metodos JPA implementados

--- bd-benchmark/src/main/java/mil/ea/cideso/satac/MotorBD.java
+++ bd-benchmark/src/main/java/mil/ea/cideso/satac/MotorBD.java
@@ -1,18 +1,35 @@
 package mil.ea.cideso.satac;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.PersistenceException;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+
 import com.google.common.base.Stopwatch;
 
 public abstract class MotorBD {
+    // Atributos comunes
     private String engineName;
     private String engineVersion;
     private String providerName;
     private String providerVersion;
     private String dbName;
+
+    // Atributos JDBC
+    private Connection conn = null;
+
+    // Atributos JPA
+    private EntityManagerFactory emf = null;
 
     // Timer
     private Stopwatch timer = null;
@@ -28,134 +45,222 @@ public abstract class MotorBD {
     // Métodos
     public abstract void createNewDatabase(String dbName) throws IOException;
 
+    public void initializeJdbcConnection(String jdbcDriver, String user, String pass, String dbUrl) {
+        try {
+            Class.forName(jdbcDriver);
+            if (getConn() == null) {
+                setConn(DriverManager.getConnection(dbUrl, user, pass));
+            }
+        } catch (ClassNotFoundException e) {
+            System.out.println("Error.");
+            System.out.println("Detalle del error: \n" + e.getMessage());
+            System.out.println("\nStacktrace:\n\n");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println("Error.");
+            System.out.println("Detalle del error: \n" + e.getMessage());
+            System.out.println("\nStacktrace:\n\n");
+            e.printStackTrace();
+        }
+    }
+
     public abstract void insertData(int cantidadAInsertar);
 
-    public abstract void readData();
+    public List<AmenazaWrapper> generateTestObjects(int cant) {
+        List<AmenazaWrapper> testObjectsList = new ArrayList<>(cant);
+        for (int i = 0; i < cant; i++) {
+            testObjectsList.add(generateAmenazaWrapper());
+        }
 
-    public abstract void updateData();
+        return testObjectsList;
+    }
 
-    public abstract void deleteData();
+    public void persistTestObjects(List<AmenazaWrapper> list) {
+        Iterator<AmenazaWrapper> listItr = list.iterator();
+        EntityManager em = getEmf().createEntityManager();
+        EntityTransaction txn = em.getTransaction();
 
-    public abstract void dropDatabase();
+        try {
+            txn.begin();
+
+            while (listItr.hasNext()) {
+                AmenazaWrapper testObject = listItr.next();
+                em.persist(testObject);
+            }
+
+            txn.commit();
+        } catch (PersistenceException e) {
+            if (txn.isActive()) {
+                txn.rollback();
+            } else {
+                System.out.println("Error.");
+                System.out.println("Detalle del error: \n" + e.getMessage());
+                System.out.println("\nStacktrace:\n\n");
+                e.printStackTrace();
+            }
+        } finally {
+            em.close();
+        }
+    }
 
     // Método para generar Amenazas (para operación INSERT)
-    // Recibe el parámetro id, el cual será establecido como el id
-    // del objeto AmenazaWrapper dentro de la BD.
-    // Devuelve una lista con todos los objetos que componen la amenaza generada.
-    public List<Object> generarAmenazaWrapper(int id) {
-        List<Object> newAmenazaList = new ArrayList<>();
-
+    public AmenazaWrapper generateAmenazaWrapper() {
         Tiempo tiempo = new Tiempo();
         tiempo.setEpoch(1);
-        newAmenazaList.add(tiempo);
 
         Posicion posicion = new Posicion();
         posicion.setLatitud(1.5);
         posicion.setLongitud(1.5);
         posicion.setMilisegundosFechaHora(1);
-        newAmenazaList.add(posicion);
 
-        Equipamiento equipamiento = new Equipamiento();
-        equipamiento.setCantidad(1);
-        equipamiento.setEquipo(1);
-        equipamiento.setTipo(1);
-        newAmenazaList.add(equipamiento);
+        Equipamiento equipamiento1 = new Equipamiento();
+        equipamiento1.setCantidad(1);
+        equipamiento1.setEquipo(1);
+        equipamiento1.setTipo(1);
 
         Equipamiento equipamiento2 = new Equipamiento();
         equipamiento2.setCantidad(1);
         equipamiento2.setEquipo(1);
         equipamiento2.setTipo(1);
-        newAmenazaList.add(equipamiento2);
 
         Equipamiento equipamiento3 = new Equipamiento();
         equipamiento3.setCantidad(1);
         equipamiento3.setEquipo(1);
         equipamiento3.setTipo(1);
-        newAmenazaList.add(equipamiento3);
-
-        List<Equipamiento> equipamientoList = new ArrayList<>();
-        equipamientoList.add(equipamiento);
-        equipamientoList.add(equipamiento2);
-        equipamientoList.add(equipamiento3);
 
         Informante informante = new Informante();
-        newAmenazaList.add(informante);
 
         Amenaza amenaza = new Amenaza();
         amenaza.setTiempo(tiempo);
-        amenaza.setCodigoSimbolo(1);
         amenaza.setPosicion(posicion);
+        amenaza.addEquipamientoItem(equipamiento1);
+        amenaza.addEquipamientoItem(equipamiento2);
+        amenaza.addEquipamientoItem(equipamiento3);
+        amenaza.setInformante(informante);
+        amenaza.setCodigoSimbolo(1);
         amenaza.setRadioAccion(1);
         amenaza.setIdentificacion(1);
         amenaza.setTamanios(1);
-        amenaza.setEquipamientoList(equipamientoList);
-        amenaza.setInformante(informante);
-        newAmenazaList.add(amenaza);
 
-        AmenazaWrapper amenazaWrapper = new AmenazaWrapper(id, amenaza, true, false);
-        newAmenazaList.add(amenazaWrapper);
+        AmenazaWrapper amenazaWrapper = new AmenazaWrapper();
+        amenazaWrapper.setAmenaza(amenaza);
+        amenazaWrapper.setVisible(true);
+        amenazaWrapper.setLeido(false);
 
-        return newAmenazaList;
+        return amenazaWrapper;
     }
 
-    // Método para generar Amenazas (para operación UPDATE)
-    // Recibe el objeto AmenazaWrapper para poder acceder a todos los
-    // objetos ligados a este (Amenaza, Tiempo, EquipamientoList, etc).
-    public void updateAmenazaWrapper(AmenazaWrapper amenazaWrapper) {
-        amenazaWrapper.getAmenaza().setCodigoSimbolo(2);
-        amenazaWrapper.getAmenaza().setRadioAccion(2);
-        amenazaWrapper.getAmenaza().setIdentificacion(2);
-        amenazaWrapper.getAmenaza().setTamanios(2);
+    public abstract void readData();
 
-        amenazaWrapper.getAmenaza().getTiempo().setEpoch(2);
+    public void readTestObjects() {
+        EntityManager em = getEmf().createEntityManager();
 
-        amenazaWrapper.getAmenaza().getPosicion().setLatitud(2.5);
-        amenazaWrapper.getAmenaza().getPosicion().setLongitud(2.5);
-        amenazaWrapper.getAmenaza().getPosicion().setMilisegundosFechaHora(2);
-
-        List<Equipamiento> equipamientoList = amenazaWrapper.getAmenaza().getEquipamientoList();
-        Iterator<Equipamiento> itrEquipamiento = equipamientoList.iterator();
-
-        while (itrEquipamiento.hasNext()) {
-            Equipamiento equip = itrEquipamiento.next();
-            equip.setCantidad(2);
-            equip.setEquipo(2);
-            equip.setTipo(2);
+        try {
+            Query query = em.createQuery("SELECT p FROM AmenazaWrapper p", AmenazaWrapper.class);
+            List results = query.getResultList();
+            Query query2 = em.createQuery("SELECT p FROM Amenaza p", Amenaza.class);
+            List results2 = query2.getResultList();
+            Query query3 = em.createQuery("SELECT p FROM Equipamiento p", Equipamiento.class);
+            List results3 = query3.getResultList();
+            Query query4 = em.createQuery("SELECT p FROM Informante p", Informante.class);
+            List results4 = query4.getResultList();
+            Query query5 = em.createQuery("SELECT p FROM Posicion p", Posicion.class);
+            List results5 = query5.getResultList();
+            Query query6 = em.createQuery("SELECT p FROM Tiempo p", Tiempo.class);
+            List results6 = query6.getResultList();
+        } catch (PersistenceException e) {
+            System.out.println("Error.");
+            System.out.println("Detalle del error: \n" + e.getMessage());
+            System.out.println("\nStacktrace:\n\n");
+            e.printStackTrace();
+        } finally {
+            em.close();
         }
-
-        amenazaWrapper.setLeido(true);
     }
 
-    // Método para devolver todos los elementos que componen una amenaza.
-    // Recibe el objeto AmenazaWrapper para poder acceder a todos los
-    // objetos ligados a este (Amenaza, Tiempo, EquipamientoList, etc).
-    // Devuelve una lista con todos los objetos que componen la amenaza.
-    public List<Object> obtenerAmenazaWrapper(AmenazaWrapper amenazaWrapper) {
+    public abstract void updateData();
 
-        List<Object> amenazaWrapperList = new ArrayList<>();
+    public void updateTestObjects() {
+        EntityManager em = getEmf().createEntityManager();
+        EntityTransaction txn = em.getTransaction();
 
-        Amenaza amenaza = amenazaWrapper.getAmenaza();
-        amenazaWrapperList.add(amenaza);
+        try {
+            txn.begin();
 
-        Tiempo tiempo = amenaza.getTiempo();
-        amenazaWrapperList.add(tiempo);
+            TypedQuery<AmenazaWrapper> query = em.createQuery("SELECT p FROM AmenazaWrapper p", AmenazaWrapper.class);
+            List<AmenazaWrapper> results = query.getResultList();
+            Iterator<AmenazaWrapper> resultsItr = results.iterator();
 
-        Informante informante = amenaza.getInformante();
-        amenazaWrapperList.add(informante);
+            while (resultsItr.hasNext()) {
+                AmenazaWrapper amenazaWrapper = resultsItr.next();
 
-        Posicion posicion = amenaza.getPosicion();
-        amenazaWrapperList.add(posicion);
+                amenazaWrapper.getAmenaza().setCodigoSimbolo(2);
+                amenazaWrapper.getAmenaza().setRadioAccion(2);
+                amenazaWrapper.getAmenaza().setIdentificacion(2);
+                amenazaWrapper.getAmenaza().setTamanios(2);
 
-        List<Equipamiento> equipamientoList = amenaza.getEquipamientoList();
-        Iterator<Equipamiento> equipamientoListItr = equipamientoList.iterator();
+                amenazaWrapper.getAmenaza().getTiempo().setEpoch(2);
 
-        while (equipamientoListItr.hasNext()) {
-            Equipamiento equip = equipamientoListItr.next();
-            amenazaWrapperList.add(equip);
+                amenazaWrapper.getAmenaza().getPosicion().setLatitud(2.5);
+                amenazaWrapper.getAmenaza().getPosicion().setLongitud(2.5);
+                amenazaWrapper.getAmenaza().getPosicion().setMilisegundosFechaHora(2);
+
+                List<Equipamiento> equipamientoList = amenazaWrapper.getAmenaza().getEquipamientoList();
+                Iterator<Equipamiento> itrEquipamiento = equipamientoList.iterator();
+                while (itrEquipamiento.hasNext()) {
+                    Equipamiento equip = itrEquipamiento.next();
+                    equip.setCantidad(2);
+                    equip.setEquipo(2);
+                    equip.setTipo(2);
+                }
+
+                amenazaWrapper.setLeido(true);
+            }
+
+            txn.commit();
+
+        } catch (PersistenceException e) {
+            System.out.println("Error.");
+            System.out.println("Detalle del error: \n" + e.getMessage());
+            System.out.println("\nStacktrace:\n\n");
+            e.printStackTrace();
+        } finally {
+            em.close();
         }
-
-        return amenazaWrapperList;
     }
+
+    public abstract void deleteData();
+
+    public void deleteTestObjects() {
+        EntityManager em = getEmf().createEntityManager();
+        EntityTransaction txn = em.getTransaction();
+
+        try {
+            txn.begin();
+
+            TypedQuery<AmenazaWrapper> query = em.createQuery("SELECT p FROM AmenazaWrapper p", AmenazaWrapper.class);
+            List<AmenazaWrapper> results = query.getResultList();
+            Iterator<AmenazaWrapper> resultsItr = results.iterator();
+            while (resultsItr.hasNext()) {
+                AmenazaWrapper testObject = resultsItr.next();
+                em.remove(testObject);
+            }
+
+            txn.commit();
+
+        } catch (PersistenceException e) {
+            System.out.println("Error.");
+            System.out.println("Detalle del error: \n" + e.getMessage());
+            System.out.println("\nStacktrace:\n\n");
+            e.printStackTrace();
+        } finally {
+            em.close();
+            if (getEmf() != null)
+                getEmf().close();
+        }
+    }
+
+    public abstract void dropDatabase();
 
     // Getters y Setters
     /**
@@ -292,6 +397,22 @@ public abstract class MotorBD {
 
     public void setStatsInsertOperation(String statsInsertOperation) {
         this.statsInsertOperation = statsInsertOperation;
+    }
+
+    public Connection getConn() {
+        return conn;
+    }
+
+    public void setConn(Connection conn) {
+        this.conn = conn;
+    }
+
+    public EntityManagerFactory getEmf() {
+        return emf;
+    }
+
+    public void setEmf(EntityManagerFactory emf) {
+        this.emf = emf;
     }
 
 }
